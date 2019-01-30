ef31ec58ffc8eed3f2f11b87a32d7395220c8e77
tiico <ockelberg@gmail.com>
2019-01-30 17:53:12 +0100

    Started adding the server functionality regarding register/login

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const config = require('./config')
 
 const app = express();
 const port = config.PORT;
 
+//Middleware
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('hejhej');
-});
+const users = require('./routes/api/users');
 
-app.use('/dev/dbtransfer', require('./routes/dev/dbtransfer'))
+//app.use('/api/users', users);
+
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+  //Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  //Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+//app.use('/dev/dbtransfer', require('./routes/dev/dbtransfer'))
 
 app.listen(port, () => {
   console.log(`Server started on ${config.URL}`)
