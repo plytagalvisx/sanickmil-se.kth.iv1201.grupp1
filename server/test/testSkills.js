b3123f910578ca280be0756a77c4b55aedbae5bf
Sabina <sabinaha@kth.se>
2019-02-13 13:20:49 +0100

    Fixed test

--- server/test/testSkills.js
+++ server/test/testSkills.js
@@ -3,26 +3,24 @@ const request = require('request');
 const config = require('./configTest');
 
 const skillUrl = config.AUTH_URL + '/api/skills';
-const userUrl = config.AUTH_URL + '/api/auth?username=emil&password=lime';
+const loginUrl = config.AUTH_URL + '/api/auth?username=emil&password=lime';
 
 /**
  * Testing the GET request for the different skills
  */
-describe("Authenticate GET request for skills", function(){
-    /**
-     * Testing the GET request when the user is logged in correctly
-     */
-    describe("GET request with successful", function(){
-        it("Returns status code 200", function(done){
-            request(userUrl, function(){
-                request(skillUrl, function(err, res){
-                    expect(res.statusCode).to.equal(200);
-                    done();
-                });
-                done();
-            });
+describe("Authenticate GET request for acquiring skills", function(){
+  /**
+  * Testing the GET request when the user is logged in correctly
+  */
+  describe("GET request with successful", function(){
+    it("Returns status code 200", function(done){
+      request(loginUrl, function(){
+        request(skillUrl, function(err, res){
+          expect(res.statusCode).to.equal(200);
+            done();
         });
-
-
+        done();
+      });
     });
+  });
 });
