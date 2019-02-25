4fd058e305dc18f12111b75291ed240442bd6ea0
Sabina <sabinaha@kth.se>
2019-02-25 13:13:24 +0100

    Commented public function

--- server/helpers/formatValidationError.js
+++ server/helpers/formatValidationError.js
@@ -1,4 +1,8 @@
 module.exports = {
+  /**
+   * Returns the validation error message without unnecessary information. 
+   * @param {The result of the validation} result 
+   */
   prettyValidation: function(result) {
     const error = result.array().map(e => {
       return {
